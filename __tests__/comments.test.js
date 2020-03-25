785e5fafd770fa6657b57f53b2301d5d7a26129e
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-24 19:41:19 -0700

    write passing PATCH route for updating a comment

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -10,7 +10,7 @@ const Tweet = require('../lib/models/Tweet');
 /*
 [X]`POST /api/v1/comments` create a comment
 [X]`GET /api/v1/comments/:id` get a comment by id and populate tweet
-[ ]`PATCH /api/v1/comments/:id` update a comment
+[X]`PATCH /api/v1/comments/:id` update a comment
 [ ]`DELETE /api/v1/comments/:id` delete a comment
 */
 
@@ -84,17 +84,23 @@ describe('comment routes', () => {
   });
 
   it('updates a comment', async() => {
+    const tweet = await Tweet.create({ 
+      handle: '@something', 
+      text: 'commentary about COVID-19' 
+    });
     const comment = await Comment.create({
+      tweetId: tweet.id,
       handle: '@commentcrazy',
       text: 'must respond to everything'
     });
-    
+
     return request(app)
-      .patch(`/api/v1/comment/${comment._id}`)
+      .patch(`/api/v1/comments/${comment._id}`)
       .send({ text: 'some dog joke' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
+          tweetId: tweet.id,
           handle: '@commentcrazy',
           text: 'some dog joke',
           __v: 0
